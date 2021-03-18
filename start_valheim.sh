13bc07aba40c2924ca955d81991d7bcd2979793a
John Violano <jv.random@pm.me>
2021-03-18 18:50:10 -0400

    valheim server really wants to be run from it's directory, and steam-run really wants a full path

--- start_valheim.sh
+++ start_valheim.sh
@@ -13,6 +13,6 @@ echo "Update complete!"
 
 echo "Starting server PRESS CTRL-C to exit"
 cd $PWD/valheim
-steam-run valheim_server.x86_64 -nographics -batchmode -name "SausageFingers Server" -port 2456 -world "SausageFingers" -password "TEMP_REPLACE_ME" -public 1
+steam-run $PWD/valheim_server.x86_64 -nographics -batchmode -name "SausageFingers Server" -port 2456 -world "SausageFingers" -password "TEMP_REPLACE_ME" -public 1
 
 export LD_LIBRARY_PATH=$templdpath
