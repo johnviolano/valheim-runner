c3e6d2adaccc772143e5cf70534372dbd2bfeca2
John Violano <jv.random@pm.me>
2021-03-18 18:14:22 -0400

    operate in place, let service make work dir

--- start_valheim.sh
+++ start_valheim.sh
@@ -1,12 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 export templdpath=$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=./linux64:$LD_LIBRARY_PATH
 export SteamAppId=892970
-export PATH=/run/current-system/sw/bin:$PATH
 
-mkdir -p /home/steam/valheim
-echo "Changing to /home/steam/valheim"
-cd /home/steam/valheim
+# need to find steamcmd/steam=run
+export PATH=/run/current-system/sw/bin:$PATH
 
 echo "Updating server..."
 steamcmd +login anonymous +force_install_dir /home/steam/valheim +app_update 896660 validate +exit
@@ -14,6 +12,6 @@ echo "Update complete!"
 
 
 echo "Starting server PRESS CTRL-C to exit"
-steam-run /home/steam/valheim/valheim_server.x86_64 -nographics -batchmode -name "SausageFingers Server" -port 2456 -world "SausageFingers" -password "TEMP_REPLACE_ME" -public 1 &
+steam-run valheim_server.x86_64 -nographics -batchmode -name "SausageFingers Server" -port 2456 -world "SausageFingers" -password "TEMP_REPLACE_ME" -public 1 &
 
 export LD_LIBRARY_PATH=$templdpath
