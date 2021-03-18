153bb2b536fcaa8a46051679d9fe1f5f1ecbdc49
John Violano <jv.random@pm.me>
2021-03-18 18:36:21 -0400

    steam really wants a fullpath

--- start_valheim.sh
+++ start_valheim.sh
@@ -7,13 +7,12 @@ export SteamAppId=892970
 export PATH=/run/current-system/sw/bin:$PATH
 
 echo "Updating server..."
-mkdir valheim
-steamcmd +login anonymous +force_install_dir valheim +app_update 896660 validate +exit
+steamcmd +login anonymous +force_install_dir $PWD/valheim +app_update 896660 validate +exit
 echo "Update complete!"
 
 
 echo "Starting server PRESS CTRL-C to exit"
-cd valheim
+cd $PWD/valheim
 steam-run valheim_server.x86_64 -nographics -batchmode -name "SausageFingers Server" -port 2456 -world "SausageFingers" -password "TEMP_REPLACE_ME" -public 1
 
 export LD_LIBRARY_PATH=$templdpath
