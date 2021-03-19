609338568ea86cfd7009a14c42f3059ffde3186b
John Violano <jv.random@pm.me>
2021-03-19 13:49:07 -0400

    Accept password file as argument

--- start_valheim.sh
+++ start_valheim.sh
@@ -10,7 +10,9 @@ echo "Updating server..."
 steamcmd +login anonymous +force_install_dir $PWD +app_update 896660 validate +exit
 echo "Update complete!"
 
+
 echo "Starting server PRESS CTRL-C to exit"
-steam-run $PWD/valheim_server.x86_64 -nographics -batchmode -name "SausageFingers Server" -port 2456 -world "SausageFingers" -password "$1" -public 1
+valheim_password=$(cat $1) # credential is passed via ExecStart = start_valheim.sh ${CREDENTIALS_DIRECTORY}/VALHEIM_PASSWORD
+steam-run $PWD/valheim_server.x86_64 -nographics -batchmode -name "SausageFingers Server" -port 2456 -world "SausageFingers" -password "$valheim_password" -public 1
 
 export LD_LIBRARY_PATH=$templdpath
