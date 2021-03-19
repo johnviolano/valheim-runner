32e87592c13d2b1654183db25d9e829f153cc20b
John Violano <jv.random@pm.me>
2021-03-19 15:09:03 -0400

    Applied patch from Jeaye to close off potential path errors

--- start_valheim.sh
+++ start_valheim.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
+set -euo pipefail
+
 export templdpath=$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=./linux64:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH="./linux64:$LD_LIBRARY_PATH"
 export SteamAppId=892970
 
 # need to find steamcmd/steam=run
-export PATH=/run/current-system/sw/bin:$PATH
+export PATH="/run/current-system/sw/bin:$PATH"
 
 echo "Updating server..."
 steamcmd +login anonymous +force_install_dir $PWD +app_update 896660 validate +exit
@@ -13,6 +15,6 @@ echo "Update complete!"
 
 echo "Starting server PRESS CTRL-C to exit"
 valheim_password=$(cat $1) # credential is passed via ExecStart = start_valheim.sh ${CREDENTIALS_DIRECTORY}/VALHEIM_PASSWORD
-steam-run $PWD/valheim_server.x86_64 -nographics -batchmode -name "SausageFingers Server" -port 2456 -world "SausageFingers" -password "$valheim_password" -public 1
+steam-run "$PWD/valheim_server.x86_64" -nographics -batchmode -name "SausageFingers Server" -port 2456 -world "SausageFingers" -password "$valheim_password" -public 1
 
 export LD_LIBRARY_PATH=$templdpath
