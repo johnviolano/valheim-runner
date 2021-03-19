1ff9b3c64da913a95573aae652d9b4f3cdd8cdf5
John Violano <jv.random@pm.me>
2021-03-19 15:14:29 -0400

    Cleanup

--- start_valheim.sh
+++ start_valheim.sh
@@ -1,20 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail
-
-export templdpath=$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH="./linux64:$LD_LIBRARY_PATH"
 export SteamAppId=892970
-
-# need to find steamcmd/steam=run
-export PATH="/run/current-system/sw/bin:$PATH"
+export PATH="/run/current-system/sw/bin:$PATH" # need to find steamcmd/steam=run
 
 echo "Updating server..."
 steamcmd +login anonymous +force_install_dir $PWD +app_update 896660 validate +exit
 echo "Update complete!"
-
-
-echo "Starting server PRESS CTRL-C to exit"
+echo "Starting server"
 valheim_password=$(cat $1) # credential is passed via ExecStart = start_valheim.sh ${CREDENTIALS_DIRECTORY}/VALHEIM_PASSWORD
 steam-run "$PWD/valheim_server.x86_64" -nographics -batchmode -name "SausageFingers Server" -port 2456 -world "SausageFingers" -password "$valheim_password" -public 1
 
-export LD_LIBRARY_PATH=$templdpath
