1f16f4207886101311036f04f7e393c75de030e2
John Violano <jv.random@pm.me>
2021-03-19 09:54:54 -0400

    .

--- start_valheim.sh
+++ start_valheim.sh
@@ -7,7 +7,7 @@ export SteamAppId=892970
 export PATH=/run/current-system/sw/bin:$PATH
 
 echo "Updating server..."
-steamcmd +login anonymous +force_install_dir $PWD/valheim +app_update 896660 validate +exit
+steamcmd +login anonymous +force_install_dir $PWD +app_update 896660 validate +exit
 echo "Update complete!"
 
 
@@ -15,7 +15,6 @@ echo "Starting server PRESS CTRL-C to exit"
 echo "Found password file at [ $VALHEIM_PASSWORD_FILE ]"
 valheim_password=$(cat $VALHEIM_PASSWORD_FILE)
 echo "Read password as [ $valheim_password ]"
-cd $PWD/valheim
 steam-run $PWD/valheim_server.x86_64 -nographics -batchmode -name "SausageFingers Server" -port 2456 -world "SausageFingers" -password "$valheim_password" -public 1
 
 export LD_LIBRARY_PATH=$templdpath
